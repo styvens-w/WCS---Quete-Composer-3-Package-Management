775163dad4acd69ad446546f8744e4297bb90f5b
Styvens Wentz <styvens.wentz@gmail.com>
2024-06-24 13:10:05 +0200

    Done

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
